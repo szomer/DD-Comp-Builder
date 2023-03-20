718290086ea61dd4e32617d59fc4e487879cd2ca
Suzanne Zomer <suzannezomer@gmail.com>
2023-03-20 17:49:34 +0100

    Fix: spelling mistake

--- darkest/src/components/more/More.js
+++ darkest/src/components/more/More.js
@@ -7,7 +7,7 @@ function More() {
             <div className='text'>
                 <h2>About</h2>
                 <p>This application is created for personal use only.</p>
-                <p>The and images data in this application comes from <a target="_blank" href="https://darkestdungeon.fandom.com/wiki/Darkest_Dungeon_Wiki_1">Darkest Dungeon Wiki</a> and <a target="_blank" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1334948457">Steam Guide</a>.</p>
+                <p>The images and data in this application comes from <a target="_blank" href="https://darkestdungeon.fandom.com/wiki/Darkest_Dungeon_Wiki_1">Darkest Dungeon Wiki</a> and <a target="_blank" href="https://steamcommunity.com/sharedfiles/filedetails/?id=1334948457">Steam Guide</a>.</p>
                 <p>Designed and developed by Suzanne Zomer. Check out the <a target="_blank" href="https://github.com/szomer/DD-Comp-Builder">GitHub page</a> for this project.</p>
             </div>
         </div>
